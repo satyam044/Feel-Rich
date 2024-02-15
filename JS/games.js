c05d14c8aa5945acc38d2b9c8c84382a65e2627d
satyam <livester044@gmail.com>
2024-02-15 17:40:43 +0530

    some part responsive

--- JS/games.js
+++ JS/games.js
@@ -33,28 +33,67 @@ let minesCnt = document.querySelector(".mines-container .mines-game");
 let numberOfMines = 25;
 let diamond = '<i class="fa-solid fa-gem"></i>';
 let bomb = '<i class="fa-solid fa-bomb"></i>';
+var selectElement = document.getElementById("mine-select");
+var minesBet = document.getElementById("mines-bet");
+var minesCashout = document.getElementById("mines-Cashout");
+
+function bombPlacement() {
+    let numberOfBombs = parseInt(selectElement.value);
+
+    let previousBombs = minesCnt.querySelectorAll(".bomb");
+    previousBombs.forEach(bombElement => {
+        bombElement.innerHTML = diamond;
+        bombElement.classList.remove("bomb");
+    });
+
+    let availableIndices = Array.from({ length: minesCnt.children.length }, (_, index) => index);
+
+    for (let i = 0; i < numberOfBombs; i++) {
+        let randomIndex = Math.floor(Math.random() * availableIndices.length);
+        let randomChildIndex = availableIndices.splice(randomIndex, 1)[0];
+
+        let randomChild = minesCnt.children[randomChildIndex];
+
+        randomChild.innerHTML = bomb;
+        randomChild.classList.add("bomb");
+    }
+}
 
 for (var i = 1; i <= numberOfMines; i++) {
     let minesBox = document.createElement("div");
     minesBox.classList.add("mines-game-box");
     minesCnt.appendChild(minesBox);
     minesBox.innerHTML = diamond;
+    let lost = false;
 
     minesBox.addEventListener("click", () => {
-        if (minesBox.classList.contains("bomb")) {
+        if (minesBox.classList.contains("bomb") && lost == false) {
             document.querySelectorAll(".mines-game-box").forEach(box => {
                 box.classList.add("mines-game-box-open");
                 box.style.opacity = "0.3"
+                lost = true;
             });
+        }
+        if (lost) {
+            minesCashout.innerText = "Bet";
+            minesCashout.addEventListener("click",function(){
+                document.querySelectorAll(".mines-game-box").forEach(box => {
+                    lost = false;
+                    bombPlacement()
+                });
+            })
         }
         minesBox.classList.add("mines-game-box-open");
         minesBox.classList.add("opacity");
         minesBox.style.opacity = "1"
+        selectElement.style.pointerEvents = "none"
+        minesBet.style.pointerEvents = "none"
+        minesCashout.style.backgroundColor = "#EF5D5D"
+
     });
 }
 
 // mines select
-var selectElement = document.getElementById("mine-select");
 var numberOfOptions = 15;
 
 for (var i = 1; i <= numberOfOptions; i++) {
@@ -62,35 +101,12 @@ for (var i = 1; i <= numberOfOptions; i++) {
     option.value = i;
     option.text = i;
     selectElement.appendChild(option);
-    var selectElement = document.getElementById("mine-select");
     var numberOfOptions = 24;
 
-    function bombPlacement() {
-        let numberOfBombs = parseInt(selectElement.value);
-
-        let previousBombs = minesCnt.querySelectorAll(".bomb");
-        previousBombs.forEach(bombElement => {
-            bombElement.innerHTML = diamond;
-            bombElement.classList.remove("bomb");
-        });
-
-        let availableIndices = Array.from({ length: minesCnt.children.length }, (_, index) => index);
-
-        for (let i = 0; i < numberOfBombs; i++) {
-            let randomIndex = Math.floor(Math.random() * availableIndices.length);
-            let randomChildIndex = availableIndices.splice(randomIndex, 1)[0];
-
-            let randomChild = minesCnt.children[randomChildIndex];
-
-            randomChild.innerHTML = bomb;
-            randomChild.classList.add("bomb");
-        }
-    }
     bombPlacement()
 
     selectElement.addEventListener("change", function () {
         bombPlacement()
     });
-
 
 }
